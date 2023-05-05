4ef4455432805c848c18ef952e4a0b061b5dd2df
seokscirbe <seokscribe1970@gmail.com>
2023-05-05 17:55:03 +0900

    함께하개 초기 프로젝트 세팅

---
+++ src/test/java/com/hanghae99/dog/DogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae99.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
