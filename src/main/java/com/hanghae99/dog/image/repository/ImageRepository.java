bd87e807799b3c349e77b80217d10b39e668bc26
seokscirbe <seokscribe1970@gmail.com>
2023-05-06 02:31:03 +0900

    AnimalNo로 Image가져오는 메서드 추가

--- src/main/java/com/hanghae99/dog/image/repository/ImageRepository.java
+++ src/main/java/com/hanghae99/dog/image/repository/ImageRepository.java
@@ -1,7 +1,11 @@
-package com.hanghae99.dog.repository;
+package com.hanghae99.dog.image.repository;
 
-import com.hanghae99.dog.entity.Image;
+import com.hanghae99.dog.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image,Long> {
+    List<Image> findByAnimal_AnimalNo(Long animalNo);
 }
