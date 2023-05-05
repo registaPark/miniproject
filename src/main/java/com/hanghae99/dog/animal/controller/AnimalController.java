f010a371984dbb5d3d4b5fbc2a1c9673febeb6de
seokscirbe <seokscribe1970@gmail.com>
2023-05-06 02:33:17 +0900

    AnimalList & Animal단건조회 컨트롤러,서비스 작성

--- src/main/java/com/hanghae99/dog/animal/controller/AnimalController.java
+++ src/main/java/com/hanghae99/dog/animal/controller/AnimalController.java
@@ -1,2 +1,28 @@
-package com.hanghae99.dog.animal.controller;public class AnimalController {
+package com.hanghae99.dog.animal.controller;
+
+import com.hanghae99.dog.animal.dto.AnimalResponseDto;
+import com.hanghae99.dog.animal.service.AnimalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class AnimalController {
+    private final AnimalService animalService;
+    @GetMapping("/list") // AniamlList 가져오기
+    public ResponseEntity<List<AnimalResponseDto>> findAnimalList(){
+        return ResponseEntity.ok(animalService.findAnimalList());
+    }
+
+    @GetMapping("/detail/{id}") //AnimalNo값으로 가져오기
+    public ResponseEntity<AnimalResponseDto> findByAnimalId(@PathVariable Long id){
+        return ResponseEntity.ok(animalService.findAnimalById(id));
+    }
 }
--- src/main/java/com/hanghae99/dog/animal/service/AnimalService.java
+++ src/main/java/com/hanghae99/dog/animal/service/AnimalService.java
@@ -1,2 +1,23 @@
-package com.hanghae99.dog.animal.service;public class AnimalService {
+package com.hanghae99.dog.animal.service;
+
+import com.hanghae99.dog.animal.dto.AnimalResponseDto;
+import com.hanghae99.dog.animal.repository.AnimalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AnimalService {
+    private final AnimalRepository animalRepository;
+
+    public List<AnimalResponseDto> findAnimalList(){ //Animal 리스트조회
+        return animalRepository.findAll().stream().map(a->AnimalResponseDto.from(a)).collect(Collectors.toList());
+    }
+
+    public AnimalResponseDto findAnimalById(Long id){ //AnimalNo로 단건조회
+        return AnimalResponseDto.from(animalRepository.findById(id).orElseThrow(()->new IllegalArgumentException("유기견을 찾을 수 없습니다.")));
+    }
 }
