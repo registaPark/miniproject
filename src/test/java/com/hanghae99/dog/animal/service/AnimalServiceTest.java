b333d78d931f1ab4f9ff81bb684d5bb8a20b2703
seokscirbe <seokscribe1970@gmail.com>
2023-05-06 02:33:36 +0900

    CI/CD위해 테스트코드 작성

--- src/test/java/com/hanghae99/dog/animal/service/AnimalServiceTest.java
+++ src/test/java/com/hanghae99/dog/animal/service/AnimalServiceTest.java
@@ -1,4 +1,76 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hanghae99.dog.animal.service;
+
+import com.hanghae99.dog.animal.dto.AnimalResponseDto;
+import com.hanghae99.dog.animal.entity.Animal;
+import com.hanghae99.dog.animal.repository.AnimalRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class AnimalServiceTest {
-  
+    @Mock
+    private AnimalRepository animalRepository;
+
+    @InjectMocks
+    private AnimalService animalService;
+
+    @Test
+    @DisplayName("유기동물 리스트 찾기 테스트")
+    public void findAnimalList(){
+        //given
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(createAnimal(1L,"말티즈"));
+        animals.add(createAnimal(2L,"포메라니안"));
+        //when
+        when(animalRepository.findAll()).thenReturn(animals);
+        //then
+        List<AnimalResponseDto> animalList = animalService.findAnimalList();
+        assertThat(animalList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("유기동물 리스트 찾기 테스트")
+    public void whenFindAnimalById(){
+        //given
+        Animal animal = createAnimal(1L, "말티즈");
+        //when
+        when(animalRepository.findById(1L)).thenReturn(Optional.of(animal));
+        //then
+        AnimalResponseDto findAnimalById = animalService.findAnimalById(1L);
+        assertThat(findAnimalById.getAnimalNo()).isEqualTo(1L);
+    }
+    @Test
+    @DisplayName("유기동물 리스트 찾기 실패 테스트")
+    public void whenFindAnimalById_thenFail(){
+        //given
+        Animal animal = createAnimal(1L, "말티즈");
+        //when
+        when(animalRepository.findById(1L)).thenReturn(Optional.empty());
+        //then
+        assertThatThrownBy(()->animalService.findAnimalById(1L)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+
+
+    private Animal createAnimal(Long animalNo , String breed) {
+        return Animal.builder()
+                .animalNo(animalNo)
+                .age("10")
+                .sex("M")
+                .breed(breed)
+                .build();
+    }
 }
