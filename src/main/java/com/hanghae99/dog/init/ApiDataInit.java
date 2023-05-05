f11d3530e5bc5ca781c7f1a93aefd4bbeb9959aa
seokscirbe <seokscribe1970@gmail.com>
2023-05-05 22:36:27 +0900

    ApiDataInit으로 컨테이너 생성후 DB에 데이터 생성 작업

--- src/main/java/com/hanghae99/dog/init/ApiDataInit.java
+++ src/main/java/com/hanghae99/dog/init/ApiDataInit.java
@@ -1,41 +1,107 @@
 package com.hanghae99.dog.init;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hanghae99.dog.entity.Animal;
+import com.hanghae99.dog.entity.Image;
+import com.hanghae99.dog.repository.AnimalRepository;
+import com.hanghae99.dog.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
+import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class DataSource {
-    private final EntityManager em;
+public class ApiDataInit {
+    private final AnimalRepository animalRepository;
+    private final ImageRepository imageRepository;
+    private final List<Animal> animalList = new ArrayList<>(); //이미지를 넣어주기 위해 리스트로 animal들을 가지고 있음.
     @PostConstruct
+    @Transactional
     public void init(){
         try {
-            URL url = new URL("http://http://openapi.seoul.go.kr:8088/6b53454b47616d6934344d7a585076/json/TbAdpWaitAnimalView//1/5");
-            HttpURLConnection conn =(HttpURLConnection) url.openConnection();
+            URL url = new URL("http://openapi.seoul.go.kr:8088/6b53454b47616d6934344d7a585076/json/TbAdpWaitAnimalView/1/28"); //유기동물 정보 받아오는 공공 api
+            HttpURLConnection conn =(HttpURLConnection) url.openConnection(); // 커넥션 연결
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept","application/json");
             if(conn.getResponseCode() != 200){
-                throw new RuntimeException("Api를 불러올수 없습니다.");
+                throw new RuntimeException("Api를 불러올수 없습니다."); //응답코드 200이 아닐시 예외 발생
             }
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            System.out.println(br.readLine());
-            System.out.println(br.readLine());
-            System.out.println(br.readLine());
-            System.out.println(br.readLine());
-            System.out.println(br.readLine());
-            System.out.println(br.readLine());
-            conn.disconnect();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(br);
+            JsonNode rows = jsonNode.path("TbAdpWaitAnimalView").path("row");
+            for (JsonNode rowNode : rows) { // Json에서 각 데이터들을 Animal엔티티로 매핑해주기 위한 작업
+                long animalId = rowNode.path("ANIMAL_NO").asLong();
+                String name = rowNode.path("NM").asText();
+                String entrncDate = rowNode.path("ENTRNC_DATE").asText();
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate entrance_date = LocalDate.parse(entrncDate, dateTimeFormatter);
+                String species = rowNode.path("SPCS").asText();
+                String breed = rowNode.path("BREEDS").asText();
+                String sex = rowNode.path("SEXDSTN").asText();
+                String age = rowNode.path("AGE").asText();
+                Float weight =Float.parseFloat(rowNode.path("BDWGH").asText());
+                String adpSttus = rowNode.path("ADP_STTUS").asText();
+                Animal animal = Animal.builder() //Animal 객체 생성
+                        .animalNo(animalId)
+                        .name(name)
+                        .entrance_date(entrance_date)
+                        .species(species)
+                        .breed(breed)
+                        .sex(sex)
+                        .age(age)
+                        .weight(weight)
+                        .adpStatus(adpSttus)
+                        .build();
+                animalList.add(animal); // Aniaml 리스트에 추가
+                animalRepository.save(animal); // Animal 저장
+            }
+            conn.disconnect(); //커넥션 리소스 해제
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }finally {
+            imageUrlSetting(); // 이미지 url DB에 넣기 위해 finally에서 실행
         }
     }
+
+    @Transactional
+    public void imageUrlSetting() {
+        for(Animal animal : animalList){
+            URL getPhotoUrl = null;
+            try {
+                getPhotoUrl = new URL("http://openapi.seoul.go.kr:8088/6b53454b47616d6934344d7a585076/json/TbAdpWaitAnimalPhotoView/1/5/?STAGE_SN=2&fileNm="+animal.getAnimalNo()); // 이미지 url 받아오기 위한 공공api
+                HttpURLConnection conn2 =(HttpURLConnection) getPhotoUrl.openConnection();
+                conn2.setRequestMethod("GET");
+                conn2.setRequestProperty("Accept","application/json");
+                if(conn2.getResponseCode() != 200){
+                    throw new RuntimeException("이미지를 불러올수 없습니다.");
+                }
+                BufferedReader imageBr = new BufferedReader(new InputStreamReader(conn2.getInputStream()));
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode jsonNode = objectMapper.readTree(imageBr);
+                JsonNode rows = jsonNode.path("TbAdpWaitAnimalPhotoView").path("row");
+                for (JsonNode row : rows) { // image엔티티로 매핑해주기 위한 작업
+                    String imageUrl = row.path("PHOTO_URL").asText();
+                    Image image = Image.builder().animal(animal).url(imageUrl).build(); // 이미지 엔티티 생성
+                    imageRepository.save(image); // 이미지 저장
+                }
+                conn2.disconnect(); // 커넥션 리소스 해제
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+
 }
