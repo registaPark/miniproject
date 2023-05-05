901bdabffd1054baec3cdda6746870793270968c
seokscirbe <seokscribe1970@gmail.com>
2023-05-06 02:29:30 +0900

    Animal & Image Dto 작성

--- src/main/java/com/hanghae99/dog/image/dto/ImageResponseDto.java
+++ src/main/java/com/hanghae99/dog/image/dto/ImageResponseDto.java
@@ -1,2 +1,13 @@
-package com.hanghae99.dog.image.dto;public class ImageResponseDto {
+package com.hanghae99.dog.image.dto;
+
+import com.hanghae99.dog.image.entity.Image;
+import lombok.Getter;
+
+@Getter
+public class ImageResponseDto {
+    private String imageUrl;
+
+    public ImageResponseDto(Image image) {
+        this.imageUrl = image.getUrl();
+    }
 }
