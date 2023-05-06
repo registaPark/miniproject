592545c87e14d9c6119f599b5fb2a2f25196eeb9
최한준 <hanjoon991@gmail.com>
2023-05-06 14:38:43 +0900

    상담신청 및 취소 1차 구축 // 레포지토리 수정 완

---
+++ src/main/java/com/hanghae99/dog/consulting/repository/ConsultingRepository.java
@@ -0,0 +1,8 @@
+package com.hanghae99.dog.consulting.repository;
+
+import com.hanghae99.dog.consulting.entity.Consulting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultingRepository extends JpaRepository<Consulting, Long> {
+
+}
