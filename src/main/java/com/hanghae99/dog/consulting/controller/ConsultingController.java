70f871857441aa4c7f6729756615dcbd41d03ba3
최한준 <hanjoon991@gmail.com>
2023-05-06 16:39:41 +0900

    ResponseEntity로 반환되게 수정 및 @CrossOrigin("*") 설정

--- src/main/java/com/hanghae99/dog/consulting/controller/ConsultingController.java
+++ src/main/java/com/hanghae99/dog/consulting/controller/ConsultingController.java
@@ -5,22 +5,26 @@ import com.hanghae99.dog.consulting.dto.ConsultingRequestsDto;
 import com.hanghae99.dog.consulting.dto.ConsultingResponseDto;
 import com.hanghae99.dog.consulting.service.ConsultingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/post/")
 public class ConsultingController {
     private final ConsultingService consultingService;
 
-
     @PostMapping("/consulting")
-    public ConsultingResponseDto applyConsulting(@RequestBody ConsultingRequestsDto dto) {
-        return consultingService.applyConsulting(dto);
+    public ResponseEntity<ConsultingResponseDto> applyConsulting(@RequestBody ConsultingRequestsDto dto) {
+        ConsultingResponseDto responseDto = consultingService.applyConsulting(dto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/consulting/{id}")
-    public CancelConsultingResponseDto cancelConsulting(@PathVariable Long id) {
-        return consultingService.cancelConsulting(id);
+    public ResponseEntity<CancelConsultingResponseDto> cancelConsulting(@PathVariable Long id) {
+        CancelConsultingResponseDto responseDto = consultingService.cancelConsulting(id);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
