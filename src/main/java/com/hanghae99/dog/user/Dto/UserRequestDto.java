c174ef4817f9b0327c75a29aaeeb98cc1f6b0039
최한준 <hanjoon991@gmail.com>
2023-05-11 16:35:22 +0900

    비밀번호 오류시 메시지 수정

--- src/main/java/com/hanghae99/dog/user/Dto/UserRequestDto.java
+++ src/main/java/com/hanghae99/dog/user/Dto/UserRequestDto.java
@@ -13,7 +13,7 @@ public class UserRequestDto {
             message = "아이디는 알파벳 소문자, 숫자를 입력하고 4~10자리로 구성해주세요.")
     private String username;
     @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~`!@#$%\\\\^&*()-])(?=\\S+$).{8,15}",
-            message = "비밀번호는 알파벳 대소문자, 숫자를 입력하고 8~15자리로 구성해주세요.")
+            message = "비밀번호는 알파벳 대소문자, 숫자를 입력하고 8~15자리로 구성해주세요. 입력된 값:")
     private String password;
     private boolean admin;
     private String adminToken;
